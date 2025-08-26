752a6a60b261c6787551e132ab412edab51d037d
tapframe <nayifveliya99@gmail.com>
2025-08-26 14:19:54 +0530

    test

--- providers/watch32.js
+++ providers/watch32.js
@@ -293,7 +293,8 @@ function extractVideostrM3u8(url) {
                             qualities,
                             headers: {
                                 'Referer': 'https://videostr.net/',
-                                'Origin': 'https://videostr.net/'
+                                'Origin': 'https://videostr.net/',
+                                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
                             }
                         }));
                 });
